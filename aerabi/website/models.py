c8a332e938ffdf3b82519787d425d571b14232d2
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2018-04-07 23:19:44 +0200

    Create about and blog pages

--- aerabi/website/models.py
+++ aerabi/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    text = models.TextField()
+    truncated = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    location = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=100, db_index=True, unique=True)
--- aerabi/website/views.py
+++ aerabi/website/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'website/index.html')
+
+
+def about(request):
+    return render(request, 'website/about.html')
+
+
+def blog(request):
+    from .models import BlogPost
+    posts = BlogPost.objects.all()
+    return render(request, 'website/blog.html', context={'posts': posts})
