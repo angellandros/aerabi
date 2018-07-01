08ffffc8e81581f6128268fdfa381c3763f11dd5
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2018-07-01 17:43:10 +0200

    Add string function to models

--- aerabi/website/models.py
+++ aerabi/website/models.py
@@ -9,7 +9,13 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     url = models.CharField(max_length=100, db_index=True, unique=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Page(models.Model):
     url = models.CharField(max_length=100, db_index=True, unique=True)
     text = models.TextField()
+
+    def __str__(self):
+        return self.url
